1144996b1d598b6c5be1f6a7979f35e951b0553c
Miguel Alvarado <miguel@nclouds.com>
2026-01-19 12:35:45 -0600

    primera version de vpc

---
+++ network/main.tf
@@ -0,0 +1,48 @@
+locals {
+
+  config = yamldecode(file("${path.module}/config/${terraform.workspace}.yaml"))
+
+  identifier = "${local.config.identifier}-${terraform.workspace}"
+
+  tags = merge(tomap({
+    Env = terraform.workspace
+  }), local.config.tags)
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
+
+  create_vpc = local.config.vpc.create_vpc
+
+  name = local.identifier
+  cidr = local.config.vpc.cidr
+
+  azs             = local.config.vpc.azs
+  private_subnets = local.config.vpc.private_subnets
+  public_subnets  = local.config.vpc.public_subnets
+  intra_subnets   = local.config.vpc.intra_subnets
+
+  enable_nat_gateway = local.config.vpc.enable_nat_gateway
+  enable_vpn_gateway = local.config.vpc.enable_vpn_gateway
+
+  single_nat_gateway     = local.config.vpc.single_nat_gateway
+  one_nat_gateway_per_az = local.config.vpc.one_nat_gateway_per_az
+
+  create_flow_log_cloudwatch_iam_role  = local.config.vpc.create_flow_log_cloudwatch_iam_role
+  create_flow_log_cloudwatch_log_group = local.config.vpc.create_flow_log_cloudwatch_log_group
+
+  enable_flow_log      = local.config.vpc.enable_flow_log
+  flow_log_file_format = local.config.vpc.flow_log_file_format
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+    "karpenter.sh/discovery"          = "${local.identifier}-eks"
+  }
+
+  tags = local.tags
+}
